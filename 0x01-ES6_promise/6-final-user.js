12e6ac77b4637f6e6fd4f2bc5af30aafc476de91
Olawole Olawale Martins <olawalemart@gmail.com>
2024-01-02 22:07:16 +0100

    all tasks

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(fileName, lastName), uploadPhoto(fileName)])
     .then((res) => {
-      const result = res.map((item) => (item.status === 'fulfilled'
-        ? { status: item.status, value: item.value }
-        : { status: item.status, value: `${item.reason}` }));
+      const result = [];
+      res.forEach((item) => (item.status === 'fulfilled'
+        ? result.push({ status: item.status, value: item.value })
+        : result.push({ status: item.status, value: `${item.reason}` })));
       return result;
     });
 }
