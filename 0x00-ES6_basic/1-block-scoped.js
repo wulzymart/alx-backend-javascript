cdcd4b2d36a37c47ad5f90ea857388c64634e0fb
Olawole Olawale Martins <olawalemart@gmail.com>
2024-01-02 22:16:11 +0100

    all tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     const task = true;
