22dd2de3fba5478e0b07e97014b376d678cb8e17
Olawole Olawale Martins <olawalemart@gmail.com>
2024-01-04 22:47:27 +0100

    es6 classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,8 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [ClassRoom(19), ClassRoom(20), ClassRoom(34)];
+  const c1 = new ClassRoom(19);
+  const c2 = new ClassRoom(20);
+  const c3 = new ClassRoom(34);
+  return [c1, c2, c3];
 }
