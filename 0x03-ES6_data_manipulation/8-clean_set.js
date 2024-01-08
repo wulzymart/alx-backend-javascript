27508a39ec19fca906ebebfa2301072806e29552
Olawole Olawale Martins <olawalemart@gmail.com>
2024-01-08 23:31:35 +0100

    es6 data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || !startString) return '';
-  return [...set].filter((element) => (element ? element.startsWith(startString) : '')).map((element) => (element ? element.slice(startString.length) : '')).join('-');
+  return [...set].filter((element) => element.startsWith(startString)).map((element) => (element.slice(startString.length))).join('-');
 }
