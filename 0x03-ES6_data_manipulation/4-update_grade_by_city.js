3f2b638c4c27d7bc418ee5835270ddb90fe4a65e
Olawole Olawale Martins <olawalemart@gmail.com>
2024-01-08 23:05:47 +0100

    es6 data manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(list, city, newGrade) {
   return list.filter((student) => student.location === city).map((student) => {
-    const grade = (newGrade.find((grade) => grade.studentId === student.id))?.grade || 'N/A';
-    return { ...student, grade };
+    const grade = (newGrade.find((grade) => grade.studentId === student.id));
+    return { ...student, grade: grade.grade || 'N/A' };
   });
 }
