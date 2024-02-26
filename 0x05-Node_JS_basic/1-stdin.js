09e30e2bb33c56d799be0dec5f2efaf22072f3ab
Olawole Olawale Martins <olawalemart@gmail.com>
2024-02-27 00:09:15 +0100

    nodejs basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,10 @@
-/**
- * get input.
- */
-function displayMessage(message) {
-  process.stdout.write(message + "\n");
-}
-
-displayMessage("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("data", (data) => {
   process.stdout.write("Your name is: " + data);
   process.exit();
 });
+
 process.on("exit", () => {
   displayMessage("This important software is now closing");
 });
