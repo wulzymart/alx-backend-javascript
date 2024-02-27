458430e91d1863ee80cc1b413e1bf83eac6aac61
Olawole Olawale Martins <olawalemart@gmail.com>
2024-02-27 01:16:08 +0100

    corrected eslint errors

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,65 +6,58 @@ const HOST = 'localhost';
 const app = http.createServer();
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
-const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  if (!dataPath) {
+const countStudents = (path) => new Promise((resolve, reject) => {
+  if (!path) {
     reject(new Error('Cannot load the database'));
   }
-  if (dataPath) {
-    fs.readFile(dataPath, (err, data) => {
+  if (path) {
+    fs.readFile(path, (err, data) => {
       if (err) {
-        reject(new Error('Cannot load the database'));
+        return reject(new Error('Cannot load the database'));
       }
-      if (data) {
-        const reportParts = [];
-        const fileLines = data.toString('utf-8').trim().split('\n');
-        const studentGroups = {};
-        const dbFieldNames = fileLines[0].split(',');
-        const studentPropNames = dbFieldNames.slice(
+      const reportParts = [];
+      const lines = data.toString('utf-8').trim().split('\n');
+      const fields = {};
+      const colTitles = lines[0].split(',');
+      const propNames = colTitles.slice(
+        0,
+        colTitles.length - 1,
+      );
+
+      lines.slice(1).forEach((line) => {
+        const entries = line.split(',');
+        const properties = entries.slice(
           0,
-          dbFieldNames.length - 1,
+          entries.length - 1,
         );
-
-        for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(',');
-          const studentPropValues = studentRecord.slice(
-            0,
-            studentRecord.length - 1,
-          );
-          const field = studentRecord[studentRecord.length - 1];
-          if (!Object.keys(studentGroups).includes(field)) {
-            studentGroups[field] = [];
-          }
-          const studentEntries = studentPropNames.map((propName, idx) => [
-            propName,
-            studentPropValues[idx],
-          ]);
-          studentGroups[field].push(Object.fromEntries(studentEntries));
+        const field = entries[entries.length - 1];
+        if (!Object.keys(fields).includes(field)) {
+          fields[field] = [];
         }
+        const studentEntries = propNames.map((propName, idx) => [
+          propName,
+          properties[idx],
+        ]);
+        fields[field].push(Object.fromEntries(studentEntries));
+      });
 
-        const totalStudents = Object.values(studentGroups).reduce(
-          (pre, cur) => (pre || []).length + cur.length,
-        );
-        reportParts.push(`Number of students: ${totalStudents}`);
-        for (const [field, group] of Object.entries(studentGroups)) {
-          reportParts.push([
-            `Number of students in ${field}: ${group.length}.`,
-            'List:',
-            group.map((student) => student.firstname).join(', '),
-          ].join(' '));
-        }
-        resolve(reportParts.join('\n'));
-      }
+      const totalStudents = Object.values(fields).reduce(
+        (pre, cur) => (pre || []).length + cur.length,
+      );
+      reportParts.push(`Number of students: ${totalStudents}`);
+      Object.entries(fields).forEach(([field, group]) => {
+        reportParts.push([
+          `Number of students in ${field}: ${group.length}.`,
+          'List:',
+          group.map((student) => student.firstname).join(', '),
+        ].join(' '));
+      });
+      return resolve(reportParts.join('\n'));
     });
   }
 });
 
-const SERVER_ROUTE_HANDLERS = [
+const handlers = [
   {
     route: '/',
     handler(_, res) {
@@ -103,12 +96,11 @@ const SERVER_ROUTE_HANDLERS = [
 ];
 
 app.on('request', (req, res) => {
-  for (const routeHandler of SERVER_ROUTE_HANDLERS) {
+  handlers.forEach((routeHandler) => {
     if (routeHandler.route === req.url) {
-      routeHandler.handler(req, res);
-      break;
+      return routeHandler.handler(req, res);
     }
-  }
+  });
 });
 
 app.listen(PORT, HOST, () => {
