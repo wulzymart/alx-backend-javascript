0c59ec247e111db4c73e07522f46137887d8a296
Olawole Olawale Martins <olawalemart@gmail.com>
2024-02-27 01:46:49 +0100

    fixed task 7

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -16,22 +16,22 @@ const MAJORS = ['CS', 'SWE'];
 class StudentsController {
   static getAllStudents(request, response) {
     const path = process.argv.length > 2 ? process.argv[2] : '';
-    const start = 'This is the list of our students\n';
+    const start = 'This is the list of our students';
 
     return readDatabase(path)
       .then((data) => {
-        let string = '';
+        const strings = [start];
         Object.entries(data).forEach(([field, students]) => {
           const firstNames = students
             .map((student) => student.firstname)
             .sort(cmp);
-          string += `Number of students in ${field}: ${
+          strings.push([`Number of students in ${field}: ${
             firstNames.length
-          }. List: ${firstNames.join(', ')}\n`;
+          }. List: ${firstNames.join(', ')}`]);
         });
-        return response.send(start + string.slice(0, -1));
+        return response.send(strings.join('\n'));
       })
-      .catch(() => response.status(500).send(`${start}Cannot load the database`));
+      .catch(() => response.status(500).send([start, 'Cannot load the database'].join('\n')));
   }
 
   static getAllStudentsByMajor(request, response) {
@@ -50,9 +50,9 @@ class StudentsController {
           const firstnames = students
             .map((student) => student.firstname)
             .sort(cmp);
-          string += `List: ${firstnames.join(', ')}\n`;
+          string = `List: ${firstnames.join(', ')}\n`;
         }
-        return response.send(start + string ? string.slice(0, -1) : '');
+        return response.send(string);
       })
       .catch(() => response.status(500).send(`${start}Cannot load the database`));
   }
