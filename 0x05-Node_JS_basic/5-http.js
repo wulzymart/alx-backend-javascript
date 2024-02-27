cd10cc64fc18ea805af2bf9318dc53bd60a10df3
Olawole Olawale Martins <olawalemart@gmail.com>
2024-02-27 01:03:20 +0100

    corrected eslint errors

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,69 +1,118 @@
 const http = require('http');
-const { readFile } = require('fs');
+const fs = require('fs');
 
-async function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    readFile(path, { encoding: 'utf-8' }, (err, dbString) => {
-      if (err) return reject(Error('Cannot load the database'));
-      const db = dbString.split('\n');
-      let colTitles;
-      let length = 0;
-      const fields = {};
-      const strings = [];
-      if (db && db[0]) {
-        colTitles = db[0].split(',');
-        if (colTitles.length !== 4) return reject(Error('Cannot load the database'));
-        if (colTitles[3] !== 'field') return reject(Error('Cannot load the database'));
-      } else return reject(Error('Cannot load the database'));
-      const entries = db.slice(1);
-      entries.forEach((entry) => {
-        if (entry) {
-          length += 1;
-          const itemArray = entry.split(',');
-          const firstName = itemArray[0];
-          const field = itemArray[3];
-          if (Object.keys(fields).includes(field)) {
-            fields[field].push(firstName);
-          } else fields[field] = [firstName];
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
+
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const reportParts = [];
+        const fileLines = data.toString('utf-8').trim().split('\n');
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(',');
+        const studentPropNames = dbFieldNames.slice(
+          0,
+          dbFieldNames.length - 1,
+        );
+
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord.slice(
+            0,
+            studentRecord.length - 1,
+          );
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+          const studentEntries = studentPropNames.map((propName, idx) => [
+            propName,
+            studentPropValues[idx],
+          ]);
+          studentGroups[field].push(Object.fromEntries(studentEntries));
         }
-      });
-      strings.push(`Number of students: ${length}`);
 
-      strings.push(`Number of students: ${length}`);
-      Object.entries(fields).forEach(([field, values]) => {
-        strings.push([
-          `Number of students in ${field}: ${values.length}.`,
-          'List:',
-          values.join(', '),
-        ].join(' '));
-      });
-      return resolve(strings.join('\n'));
+        const totalStudents = Object.values(studentGroups).reduce(
+          (pre, cur) => (pre || []).length + cur.length,
+        );
+        reportParts.push(`Number of students: ${totalStudents}`);
+        for (const [field, group] of Object.entries(studentGroups)) {
+          reportParts.push([
+            `Number of students in ${field}: ${group.length}.`,
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        resolve(reportParts.join('\n'));
+      }
     });
-  });
-}
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      countStudents(process.argv[2].toString())
-        .then((string) => {
-          res.end(string.slice(0, -1));
+  }
+});
+
+const SERVER_ROUTE_HANDLERS = [
+  {
+    route: '/',
+    handler(_, res) {
+      const responseText = 'Hello Holberton School!';
+
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    },
+  },
+  {
+    route: '/students',
+    handler(_, res) {
+      const responseParts = ['This is the list of our students'];
+
+      countStudents(DB_FILE)
+        .then((report) => {
+          responseParts.push(report);
+          const responseText = responseParts.join('\n');
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseText.length);
+          res.statusCode = 200;
+          res.write(Buffer.from(responseText));
         })
-        .catch(() => {
-          res.statusCode = 500;
-          res.end('Cannot load the database');
+        .catch((err) => {
+          responseParts.push(err instanceof Error ? err.message : err.toString());
+          const responseText = responseParts.join('\n');
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseText.length);
+          res.statusCode = 200;
+          res.write(Buffer.from(responseText));
         });
-    } catch {
-      res.statusCode = 500;
-      res.end('Cannot load the database');
+    },
+  },
+];
+
+app.on('request', (req, res) => {
+  for (const routeHandler of SERVER_ROUTE_HANDLERS) {
+    if (routeHandler.route === req.url) {
+      routeHandler.handler(req, res);
+      break;
     }
   }
 });
 
-app.listen(1245, '127.0.0.1');
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
