790106c1e9d3ed9f61dd050da8f56b2c2e636b29
Olawole Olawale Martins <olawalemart@gmail.com>
2024-02-29 15:51:33 +0100

    js testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,27 +1,32 @@
 const express = require('express');
 
 const app = express();
-
 const PORT = 7865;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id(\\d+)', (req, res) => {
   const { id } = req.params;
-  return res.send(`Payment methods for cart ${id}`);
+
+  res.send(`Payment methods for cart ${id}`);
 });
+
 app.get('/available_payments', (_req, res) => {
   res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
 app.post('/login', (req, res) => {
-  const userName = req.body?.userName || '';
+  let username = '';
 
-  res.send(`Welcome ${userName}`);
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
 });
 
 app.listen(PORT, () => {
